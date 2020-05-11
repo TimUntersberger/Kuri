4f93d1c2b2f81a8ee62de510ea5bcb08a2c3f771
TimUntersberger <timuntersberger2@gmail.com>
2020-05-12 00:11:07 +0200

    Remove semicolon

--- lib/index.ts
+++ lib/index.ts
@@ -37,7 +37,7 @@ function createQuery(sql: string){
 }
 
 type Driver = "psql"
-let driver: Driver | null = null;
+let driver: Driver | null = null
 
 type SubType<Base, Condition> = Pick<Base, {
     [Key in keyof Base]: Base[Key] extends Condition ? Key : never
